a482b0d16433e338f8ed735926c0846cb9001072
Aliff <aliff.macapinlac@gmail.com>
2020-04-18 15:23:37 -0700

    added logo

--- wp-content/themes/rwfho/header.php
+++ wp-content/themes/rwfho/header.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"><img id="brand" src="<?php echo get_theme_file_uri('/assets/images/logo-brand.png') ?>"/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
