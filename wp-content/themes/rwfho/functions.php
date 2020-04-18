e4189ae0d4d3d65afcc562693c698807f95aeb15
Aliff <aliff.macapinlac@gmail.com>
2020-04-18 14:24:05 -0700

    added bootstrap nav bar and made some config changes to get ready to put on aws

--- wp-content/themes/rwfho/functions.php
+++ wp-content/themes/rwfho/functions.php
@@ -2,8 +2,8 @@
 
 
     function rwfho_files() {
-        echo get_stylesheet_uri();
         wp_enqueue_style('rwfho_main_style', get_stylesheet_uri());
+        wp_enqueue_style('rwfho_bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css");
     }
     add_action('wp_enqueue_scripts', 'rwfho_files'); 
 
