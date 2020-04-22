f472eed7eed1b567a92b52d7804e20513c9d0dc0
Aliff <aliff.macapinlac@gmail.com>
2020-04-22 00:58:23 -0700

    did more work

--- wp-content/themes/rwfho/header.php
+++ wp-content/themes/rwfho/header.php
@@ -15,24 +15,21 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/rwfho">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Resources</a>
+        <a class="nav-link" href="opportunities">Opportunities</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Resources
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="resources" >Resource 1</a>
+          <a class="dropdown-item" href="https://instabio.cc/realwfhjobs" target="_blank">RealWFHjobs</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">R3</a>
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
--- wp-content/themes/rwfho/page.php
+++ wp-content/themes/rwfho/page.php
@@ -2,7 +2,22 @@
     get_header();
     while(have_posts()) {
         the_post();?>
-        <h1><?php the_title()?></h1>
+        <h1><?php 
+            $title=get_the_title(); 
+            echo $title;
+            if(strtolower($title) == 'opportunities') {
+                ?>
+                <h1>Opportunities Stuff goes here</h1>
+                <?php
+            } else {
+                ?>
+
+                <h1>Non Opportunities Pages here</h1>
+
+                <?php
+            }
+        ?>
+        </h1>
         <?php the_content(); ?>
  <?php   } 
  get_footer(); 
